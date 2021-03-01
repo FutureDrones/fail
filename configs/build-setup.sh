cb9cb279ed6a22eec203c68562e35c6ee928e9ae
JamieHoSzeYui <henloboii@protonmail.ch>
2021-03-01 03:30:59 +0000

    configs: build: use ccache

--- configs/build-setup.sh
+++ configs/build-setup.sh
@@ -2,5 +2,10 @@
 
 apt-get -y update && apt-get -y upgrade && apt-get -y install bc build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev repo rsync flex bison
 apt-get -y install nano ccache git screen
-
+export ARCH_ARM_HAVE_TLS_REGISTER=true
+export TARGET_CPU_SMP=true
+export WITH_JIT=true
+export JS_ENGINE=v8 
+export USE_CCACHE=1
+ccache -M 50G
 echo "Ready. Let's goooooooooooooooooooooo"
